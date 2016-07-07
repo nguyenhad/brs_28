6625a2ce3ea28eb64d458d114f59016a6c3ff273
cuongncFramgia <nguyen.chi.cuong@framgia.com>
2016-07-07 13:38:05 +0700

    devise_gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+  include CanCan::ControllerAdditions
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
   end
-
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.for :sign_up do |user|
