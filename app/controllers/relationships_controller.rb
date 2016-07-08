fb13b2563685c33e002ea8437939890f6cf26501
NghiaNTT <nguyen.thi.trong.nghia@framgia.com>
2016-07-08 17:33:08 +0700

    following-user

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,37 @@
+class RelationshipsController < ApplicationController
+  def index
+    @user = User.find_by id: params[:id]
+    path = params[:path]
+    @title = t "views.users.#{path}"
+    @users = @user.send(path).order(updated_at: :desc).page(params[:page])
+      .per Settings.per_page
+  end
+
+  def create
+    @user = User.find_by id: params[:followed_id]
+    if @user.present?
+      current_user.follow @user
+      respond_to do |format|
+        format.html {redirect_to @user}
+        format.js
+      end
+    else
+      flash.now[:danger] = t "views.users.invalid_user"
+      redirect_to users_path
+    end
+  end
+
+  def destroy
+    @user = Relationship.find_by id: (params[:id]).followed
+    if @user.present?
+      current_user.unfollow @user
+      respond_to do |format|
+        format.html {redirect_to @user}
+        format.js
+    else
+      flash.now[:danger] = t "views.users.invalid_user"
+      redirect_to users_path
+      end
+    end
+  end
+end
