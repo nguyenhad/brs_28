4d3beba9c3bfbab75e796269f4291c5fd5be4230
Nguyen Thi Thu Ha D <nguyen.thi.thu.had@gmail.com>
2016-07-09 14:24:45 +0700

    admin search category
    admin search category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -5,12 +5,28 @@ class Admin::CategoriesController < ApplicationController
   before_action :check_exist_books, only: :destroy
 
   def index
-    @categories = Kaminari.paginate_array(Category.all.order(created_at: :desc))
-      .page(params[:page]).per Settings.per_page
+    @search = Category.search(params[:q])
+    @categories = @search.result.page(params[:page]).per Settings.per_page
+    if @categories.blank?
+      flash[:alert] =
+        t "controllers.admin.categories.flash.alert.search_category"
+      redirect_to admin_categories_path
+    end
   end
 
   def new
     @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] =
+        t "controllers.admin.categories.flash.success.create_category"
+      redirect_to admin_categories_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,26 +34,18 @@ class Admin::CategoriesController < ApplicationController
 
   def update
     if @category.update_attributes category_params
-      flash[:success] = t "controllers.admin.categories.flash.success.edit_category"
+      flash[:success] =
+        t "controllers.admin.categories.flash.success.edit_category"
       redirect_to admin_categories_path
     else
       render :edit
     end
   end
 
-  def create
-    @category = Category.new category_params
-    if @category.save
-      flash[:success] = t "controllers.admin.categories.flash.success.create_category"
-      redirect_to admin_categories_path
-    else
-      render :new
-    end
-  end
-
   def destroy
     @category.destroy
-    flash[:success] = t "controllers.admin.categories.flash.success.delete_category"
+    flash[:success] =
+      t "controllers.admin.categories.flash.success.delete_category"
     redirect_to admin_categories_path
   end
 
@@ -48,8 +56,8 @@ class Admin::CategoriesController < ApplicationController
 
   def check_exist_books
     if @category.books.any?
-      flash[:danger] = t "controllers.admin.categories.flash.danger.delete_category"
-      redirect_to admin_categories_path
+      flash[:danger] =
+        t "controllers.admin.categories.flash.danger.delete_category"
     end
   end
 end
