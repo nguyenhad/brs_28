147e8c6c29b1162bb7dcc3ffb46e461351e00833
cuongncFramgia <nguyen.chi.cuong@framgia.com>
2016-07-19 11:37:35 +0700

    delete_comment_by_ajax

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -3,11 +3,15 @@ module CommentHelper
     data = Hash.new
     data[:container] = "comment_list_" + comment.review.id.to_s
     data[:id] = comment.id
-    data[:ava] = comment.user.avatar.url.to_s
+    if comment.user.avatar.present?
+      data[:ava] = comment.user.avatar.url.to_s
+    else
+      data[:ava] = "/assets/" + Settings.users.default_avatar
+    end
     data[:user_name] = comment.user.name
     data[:user_url] = "users/" + comment.user.id.to_s
     data[:content] = comment.content
-    data[:time] = comment.created_at.strftime("%d-%m-%Y %I:%M %p")
+    data[:time] = comment.created_at.strftime t("views.comments.time_format")
     return data
   end
 end
